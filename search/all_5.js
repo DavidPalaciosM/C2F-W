17846bd3a73223da832a5bbd3f320a4d70072bb6
Fdo Bad Vel <fbadilla@ing.uchile.cl>
2025-02-19 18:22:28 +0000

    Deploy to GitHub Pages

---
+++ search/all_5.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['detailed_20behavior_3a_0',['detailed behavior:',['../dc/d0a/classCell2Fire.html#autotoc_md2',1,'Detailed Behavior:'],['../dc/d0a/classCell2Fire.html#autotoc_md5',1,'Detailed Behavior:'],['../dc/d0a/classCell2Fire.html#autotoc_md9',1,'Detailed Behavior:']]],
+  ['details_3a_1',['Details:',['../dc/d0a/classCell2Fire.html#autotoc_md1',1,'']]],
+  ['downstream_20protection_20value_20dpv_20and_20growth_20propagation_20tree_20gpt_2',['Risk metrics: Burn Probability (BP), Betweenness Centrality (BC), Downstream Protection Value (DPV), and Growth Propagation Tree (GPT).',['../index.html#autotoc_md26',1,'']]],
+  ['dpv_20and_20growth_20propagation_20tree_20gpt_3',['Risk metrics: Burn Probability (BP), Betweenness Centrality (BC), Downstream Protection Value (DPV), and Growth Propagation Tree (GPT).',['../index.html#autotoc_md26',1,'']]],
+  ['dr_4',['Usage (TL;DR)',['../index.html#autotoc_md23',1,'']]]
+];
---
+++ search/functions_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['get_5fburned_0',['get_burned',['../d4/dcd/classCells.html#a0dfa763834bd94293d1999e520efffd6',1,'Cells']]],
+  ['getfireprogressmatrix_1',['getFireProgressMatrix',['../dc/d0a/classCell2Fire.html#a34af45069be4a20bda38dc2c8c007cbd',1,'Cell2Fire']]],
+  ['getmessages_2',['GetMessages',['../dc/d0a/classCell2Fire.html#a31931e30982982168b82824efbb5b43a',1,'Cell2Fire']]],
+  ['getrosmatrix_3',['getROSMatrix',['../dc/d0a/classCell2Fire.html#a2e71aae663a9f4c1ffae25a00bc754e8',1,'Cell2Fire']]],
+  ['getstatus_4',['getStatus',['../d4/dcd/classCells.html#ad99bb83f69f9966b9f6e88ad1626f4f4',1,'Cells']]]
+];
